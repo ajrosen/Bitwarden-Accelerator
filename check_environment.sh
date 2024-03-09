4174f321e0cf285362b143f71f14a0a292b1e32e
Andy Rosen <ajr@corp.mlfs.org>
2024-03-09 13:31:15 -0500

    Performance improvement

--- check_environment.sh
+++ check_environment.sh
@@ -9,22 +9,20 @@
 [ -x "${alfred_workflow_cache}/jq" ] || ./install_dependency.sh "JQ" "jq" "jq"
 
 # Get current settings
-cat > "${alfred_workflow_data}"/bw.vars  << EOF
-old_SyncTime=${SyncTime}
-old_autoSync=${autoSync}
-EOF
+# cat > "${alfred_workflow_data}"/bw.vars  << EOF
+# old_SyncTime=${SyncTime}
+# old_autoSync=${autoSync}
+# EOF
 
 # Load old settings
-[ -f "${alfred_workflow_data}"/bw.vars.old ] || mv -f "${alfred_workflow_data}"/bw.vars "${alfred_workflow_data}"/bw.vars.old
-. "${alfred_workflow_data}"/bw.vars.old
+# [ -f "${alfred_workflow_data}"/bw.vars.old ] || mv -f "${alfred_workflow_data}"/bw.vars "${alfred_workflow_data}"/bw.vars.old
+# . "${alfred_workflow_data}"/bw.vars.old
 
-# Check changes to sync agent
-if [ "${autoSync}" != "${old_autoSync}" ] || [ "${SyncTime}" != "${old_SyncTime}" ]; then
-    . ./install_sync_agent.sh
-fi
+# Check sync agent
+. ./install_sync_agent.sh
 
 # Save current settings
-mv -f "${alfred_workflow_data}"/bw.vars "${alfred_workflow_data}"/bw.vars.old
+# mv -f "${alfred_workflow_data}"/bw.vars "${alfred_workflow_data}"/bw.vars.old
 
 # Pass input as output
 echo -n "${*}"
