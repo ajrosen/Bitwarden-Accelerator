7fcdb9e5623ffd82a792e9e44b481ce4adbdd4c5
Andy Rosen <ajr@corp.mlfs.org>
2024-07-19 18:40:19 -0400

    Use the epoch if .lastSync is undefined

--- lib/status.sh
+++ lib/status.sh
@@ -10,6 +10,6 @@ if [ ! -s "${STATUS_FILE}" ]; then
     return
 fi
 
-export SYNC=$(jq -r '.lastSync | "\(.[0:19])Z" | fromdate | strflocaltime("%c %Z")' "${STATUS_FILE}")
+export SYNC=$(jq -r '.lastSync // "1970-01-01T00:00:00.000Z" | "\(.[0:19])Z" | fromdate | strflocaltime("%c %Z")' "${STATUS_FILE}")
 export STATE=$(jq -r '.status' "${STATUS_FILE}")
 export USER=$(jq -r '.userEmail' "${STATUS_FILE}")
